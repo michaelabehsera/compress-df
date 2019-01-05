e0820702ab6e23b33d72f9dba8143d4d603d3145
Michael Abeshera <michaelabeshera@Michaels-MacBook-Pro.local>
2019-01-04 17:24:24 -0800

    update plugin

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Compress DataFrame, is a script that will automatically optimize your DataFrame in order to reduce its size in some cases by up to 70%. Note: It only works on numerical variables not Object based ones. Support for more types of variables and other optimization functions coming soon",
     long_description='Compress DataFrame will automatically pick the correct dtypes based on each column and find the one that takes up the least memory',
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/michaelabehsera/compress-df",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
